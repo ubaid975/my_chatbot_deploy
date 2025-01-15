3e12abd4936b4bfcb0be3470ce4fa65404fded6c
ubaid memon <154630928+ubaid975@users.noreply.github.com>
2025-01-15 18:32:06 +0500

    Update app.py

--- app.py
+++ app.py
@@ -62,6 +62,6 @@ def chat(text,history):
         return 'your internet issue'
 
 app=ChatInterface(fn=chat,theme=gr.themes.Ocean())
-app.launch(server_name="0.0.0.0")
+app.launch(server_name="fcmbikgfcmibp7szw84qnl.streamlit.app/")
 
 
