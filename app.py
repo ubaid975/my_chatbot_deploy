0b12b5daf4899a191b1e2051714bbfddb5c8f8cd
ubaid memon <154630928+ubaid975@users.noreply.github.com>
2025-01-15 18:55:35 +0500

    Update app.py

--- app.py
+++ app.py
@@ -71,10 +71,7 @@ def chat(text,history):
 app=ChatInterface(fn=chat,theme=gr.themes.Ocean())
 def run_gradio():
     app.launch(show_error=True, share=False)
-if "gradio_thread" not in st.session_state:
-    st.session_state["gradio_thread"] = Thread(target=run_gradio, daemon=True)
-    st.session_state["gradio_thread"].start()
-    time.sleep(2)
+run_gradio()
 st.components.v1.iframe(src="http://127.0.0.1:7860/", width=800, height=600)
 
 
